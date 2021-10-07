42a6012e9e10b0b07df844720a4a085302bfb283
Ye0078 <70047423+Ye0078@users.noreply.github.com>
2021-10-07 07:59:16 +0630

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ var x = setInterval(function() {
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "Happy Birthdayပါ🙆🙆";
   }
 }, 1000);
 

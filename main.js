ee6f637824fa4c928e1f76faa039a212fe0495ca
Ye0078 <70047423+Ye0078@users.noreply.github.com>
2021-12-26 12:29:39 +0630

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("t 10,  24:00:00").getTime();
+var countDownDate = new Date(" 10,  24:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -28,7 +28,7 @@ var x = setInterval(function() {
 }, 1000);
 
 // clac days
-var firstDay = new Date("2021-09-07");
+var firstDay = new Date("2021-11-16");
 var now = new Date();
 var toFirst = firstDay.getTime();
 var toNow = now.getTime();
